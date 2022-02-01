ec7e7bfad191f2dc22138cdabe6f3efe7750738a
yfuoft <yousuf.farhan@mail.utoronto.ca>
2022-02-01 15:42:49 -0500

    Commit 2

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"text/template"
+
 	"github.com/gorilla/mux"
 )
 type Posts []Post
-
+var templates  *template.Template
 func main(){ 
+	templates = template.Must(template.ParseGlob("templates/*.html"))
 	router := mux.NewRouter()
+	router.HandleFunc("/", indexHandler)
+	http.Handle("/", router)
+	http.ListenAndServe(":8081", nil)
 
 }
+func indexHandler(res http.ResponseWriter, req *http.Request){
+	templates.ExecuteTemplate(res, "index.html", nil)
 
+}
 type Post struct {
 	Title string `json:title`
 	Body string `json:body`
